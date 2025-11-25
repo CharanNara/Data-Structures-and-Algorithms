0b0c45f3442bb8d99b42e8d6f498b6129e70d3be
Charan Nara <46029775+CharanNara@users.noreply.github.com>
2025-11-25 12:52:00 -0500

    Time: 341 ms (5.24%) | Memory: 28.4 MB (10.28%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.py
@@ -0,0 +1,29 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        directions = [[0,1],[1,0],[0,-1],[-1,0]]
+        rows, cols = len(grid), len(grid[0])
+
+        # from one position you explore all possible directions where island is connected
+
+        visited = set()
+
+        def explore(r, c):
+            
+            visited.add((r,c))
+
+            for dr,dc in directions:
+                nr, nc = dr+r, dc+c
+
+                if nr in range(rows) and nc in range(cols) and (nr, nc) not in visited and grid[nr][nc] == "1":
+                    explore(nr, nc)
+                    visited.add((nr, nc))
+
+
+        res = 0
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    explore(r, c)
+                    res += 1
+        return res
